0434a57c476971f67ed26cefc17012a593346f15
Paul Kramer <pk@cantamen.de>
2021-03-01 15:15:51 +0100

    moved the alarm listener to the basic botlib

--- src/main/java/de/gitterrost4/idleonbot/config/containers/ServerConfig.java
+++ src/main/java/de/gitterrost4/idleonbot/config/containers/ServerConfig.java
@@ -3,10 +3,8 @@ package de.gitterrost4.idleonbot.config.containers;
 import java.util.Optional;
 
 import de.gitterrost4.botlib.config.containers.modules.ModuleConfig;
-import de.gitterrost4.idleonbot.config.containers.modules.AlarmConfig;
 import de.gitterrost4.idleonbot.config.containers.modules.RecipeConfig;
 import de.gitterrost4.idleonbot.config.containers.modules.WikiConfig;
-import de.gitterrost4.idleonbot.listeners.AlarmListener;
 import de.gitterrost4.idleonbot.listeners.RecipeListener;
 import de.gitterrost4.idleonbot.listeners.WikiListener;
 import net.dv8tion.jda.api.JDA;
@@ -16,11 +14,10 @@ public class ServerConfig extends de.gitterrost4.botlib.config.containers.Server
   
   private RecipeConfig recipeConfig;
   private WikiConfig wikiConfig;
-  private AlarmConfig alarmConfig;
   
   @Override
   public String toString() {
-    return "ServerConfig [recipeConfig=" + recipeConfig + ", wikiConfig=" + wikiConfig + ", alarmConfig=" + alarmConfig
+    return "ServerConfig [recipeConfig=" + recipeConfig + ", wikiConfig=" + wikiConfig + ", alarmConfig=" + getAlarmConfig()
         + ", getSuperUserRoles()=" + getSuperUserRoles() + ", getName()=" + getName() + ", getServerId()="
         + getServerId() + ", getBotPrefixes()=" + getBotPrefixes() + ", getDatabaseFileName()=" + getDatabaseFileName()
         + ", getHelpConfig()=" + getHelpConfig() + "]";
@@ -34,10 +31,6 @@ public class ServerConfig extends de.gitterrost4.botlib.config.containers.Server
     return wikiConfig;
   }
 
-  public AlarmConfig getAlarmConfig() {
-    return alarmConfig;
-  }
-
   @Override
   protected void addServerModules(JDA jda, Guild guild, de.gitterrost4.botlib.listeners.ListenerManager manager) {
     if (Optional.ofNullable(getRecipeConfig()).map(ModuleConfig::isEnabled).orElse(false)) {
@@ -45,9 +38,6 @@ public class ServerConfig extends de.gitterrost4.botlib.config.containers.Server
     }
     if (Optional.ofNullable(getWikiConfig()).map(ModuleConfig::isEnabled).orElse(false)) {
       manager.addEventListener(new WikiListener(jda, guild, this));
-    }
-    if (Optional.ofNullable(getAlarmConfig()).map(ModuleConfig::isEnabled).orElse(false)) {
-      manager.addEventListener(new AlarmListener(jda, guild, this));
     }
   }
 
--- src/main/java/de/gitterrost4/idleonbot/config/containers/modules/AlarmConfig.java
+++
@@ -1,7 +0,0 @@
-package de.gitterrost4.idleonbot.config.containers.modules;
-
-import de.gitterrost4.botlib.config.containers.modules.CommandModuleConfig;
-
-public class AlarmConfig extends CommandModuleConfig {
-
-}
