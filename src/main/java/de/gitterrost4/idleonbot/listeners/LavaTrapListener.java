6bf5bf03e9263ea63eba15434648f86824df0ae0
Paul Kramer <pk@cantamen.de>
2021-03-02 17:36:33 +0100

    fixing a trap

--- src/main/java/de/gitterrost4/idleonbot/listeners/LavaTrapListener.java
+++ src/main/java/de/gitterrost4/idleonbot/listeners/LavaTrapListener.java
@@ -1,6 +1,5 @@
 package de.gitterrost4.idleonbot.listeners;
 
-import de.gitterrost4.botlib.config.containers.modules.ModuleConfig;
 import de.gitterrost4.botlib.listeners.AbstractListener;
 import de.gitterrost4.idleonbot.config.containers.ServerConfig;
 import net.dv8tion.jda.api.JDA;
@@ -17,7 +16,7 @@ public class LavaTrapListener extends AbstractListener<ServerConfig> {
   public void onGuildMemberJoin(GuildMemberJoinEvent event) {
     super.onGuildMemberJoin(event);
     if(event.getUser().getId().equals("282657635067494410") || event.getUser().getId().equals("603659239428194317")) {
-      guild().getTextChannelById("813059416424448030").sendMessage(event.getMember().getAsMention()+" We caught you, Lava! Now @ everyone knows you're here!").queue(); 
+      guild().getTextChannelById("813059416424448030").sendMessage(event.getMember().getAsMention()+" We caught you, Lava! Now @everyone knows you're here!").queue(); 
     }
   }
   
